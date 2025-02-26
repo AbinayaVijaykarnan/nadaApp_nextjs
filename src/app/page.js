66d7923530447a1c8a03a98b13ccc0954de7a499
Abinaya Kumari Karthikeyan <akkarthikeyan@tekion.com>
2025-02-26 17:21:05 +0530

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,161 @@
-import Image from "next/image";
+'use client'; // Add this to indicate that this is a Client Component
+
+import React, { useState, useEffect } from 'react';
+import { Button, Modal, Select, Radio , Checkbox } from 'antd';
+// import Button from 'tcomponents/atoms/Button/Button';
 import styles from "./page.module.css";
+import CreateModule from './createModule';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [showModal, setshowModal] = useState(false);
+  const [showAppDrawer, setShowAppDrawer] = useState(false);
+  const [selectedAppName, setSelectedAppName] = useState('none');
+  const [selectedFieldType, setSelectedFieldType] = useState('all_fields');
+  const isCustomFields = selectedFieldType === 'custom_fields';
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  // useEffect(()=>{
+  // return () =>{
+  //   console.log('Home component  unmount');
+  //   setSelectedAppName('none');
+  //   setSelectedAppName('all_fields');
+  // }
+  // },[]);
+
+  const appOptions = [
+    {
+      value: 'none',
+      label: 'None',
+    },
+    {
+      value: 'salesforce',
+      label: 'Salesforce',
+    },
+    {
+      value: 'netsuite',
+      label: 'Netsuite',
+    },
+    {
+      value: 'tm',
+      label: 'Task Management',
+    },
+  ];
+
+  const radiooptions = [
+    {
+      value: 'all_fields',
+      label: 'All Fields',
+    },
+    {
+      value: 'custom_fields',
+      label: 'Custom Fields',
+    },
+  ]
+
+  const onCreateObjectClick = () => {
+    console.log('onCreateObjectClick');
+    setshowModal(true);
+  };
+
+
+  const handleModalOk = () => {
+    console.log('handleModalOk');
+    setshowModal(false);
+    // setSelectedAppName('none');
+    // setSelectedFieldType('all_fields');
+    setShowAppDrawer(true);
+  };
+
+  const handleModalCancel = () => {
+    console.log('handleModalCancel');
+    setshowModal(false);
+  };
+
+  const onSelectAppName = (appName) => {
+    console.log('onSelectAppName', appName);
+    setSelectedAppName(appName);
+  };
+
+  const onSelectFieldType = (e) => {
+    const val = e.target.value;
+    console.log('onSelectFieldType', val);
+    setSelectedFieldType(val);
+  };
+
+  const onSelectCheckbox = (val) => {
+    console.log('onSelectCheckbox', val);
+  };
+
+
+  const getCheckBoxOptions = (appName) =>{ // TODO: Later do api call, and get checkbox options (i.e: field names) by application name
+    let options = [];
+    switch(appName){
+      case 'salesforce' : options = ['dealer_name', 'dealer_group_name', 'status', 'email_id', 'products'];
+      return;
+      case 'tm' : options = ['']
+    }
+
+  };
+
+  console.log({ showModal, selectedAppName , selectedFieldType});
+
+  return (
+    <div className={styles.mainPage}>
+      <h2 className={styles.headerName}>Module App</h2>
+      <Button
+        id="create_object"
+        type="primary"
+        className={styles.createObjButton}
+        // disabled={commentsContent?.comment_id === data.activity_comment_id}
+        onClick={(e) => { onCreateObjectClick() }}>+ Create Object
+      </Button>
+      <Modal
+        width={{
+          xs: "90%",
+          sm: "80%",
+          md: "70%",
+          lg: "60%",
+          xl: "50%",
+          xxl: "40%",
+        }}
+        // styles={{minHeight: '30rem'}}
+        className={styles.appModal}
+        title="Application Modal"
+        open={showModal}
+        onOk={handleModalOk}
+        onCancel={handleModalCancel}
+      // okButtonProps={{ disabled: true }}
+      // cancelButtonProps={{ disabled: true }}
+      >
+        <div className={styles.firstDiv}>
+          <div className={styles.label}>Application Name</div>
+          <Select
+            //  style={{ width: '100%' }}
+            className={styles.appSelectBox}
+            showSearch
+            placeholder="Select Application"
+            optionFilterProp="label"
+            onChange={onSelectAppName}
+            onSearch={(val) => { console.log('search val', val) }}
+            value={selectedAppName}
+            options={appOptions}
+          />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.secondDiv}>
+        <div className={styles.label}>Field Type</div>
+        <Radio.Group options={radiooptions} defaultValue={selectedFieldType} onChange={onSelectFieldType}/>
+        {
+          isCustomFields && (
+           <div>
+             <div className={styles.label}>Application Name</div>
+             <Checkbox.Group options={getCheckBoxOptions(selectedAppName)} defaultValue={['Apple']} onChange={onSelectCheckbox} />
+            </div>
+          )
+        }
+        </div>
+      </Modal>
+      {
+        showAppDrawer && <CreateModule showAppDrawer={showAppDrawer} setShowAppDrawer={setShowAppDrawer} selectedAppName={selectedAppName} />
+      }
     </div>
   );
 }
